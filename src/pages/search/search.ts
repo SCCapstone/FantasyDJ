0b40e32c145dc67f5b0436bd1757c32b37574bbc
Thomas Brower <browert2@email.sc.edu>
2016-11-22 10:09:30 -0500

    fix nuke

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { SpotifyProvider } from '../../providers/spotify-provider';
 
 /*
   Generated class for the Search page.
-
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html'
 })
 export class SearchPage {
+public searchInput: any;
+public tracks:any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private spotify: SpotifyProvider) {
+  	this.searchInput = "TEST";
+  
+  	console.log(this.searchInput);
+
+  }
+
 
   ionViewDidLoad() {
-    console.log('Hello Search Page');
+    console.log('Landed on Search Page');
+  }
+
+  doSearch(val) {
+  console.log(this.searchInput);
+  this.spotify.search(this.searchInput).then(res => {
+	 	this.tracks = res.tracks;
+	    }, err => {
+	      console.log('error searching spotify');
+	    });
   }
 
 }

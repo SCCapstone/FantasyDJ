9ecbfab16a045a0a325fedc4c17813bef9d94ab5
Philip Oliver-Paull <oliverpa@mailbox.sc.edu>
2017-01-27 00:07:32 -0500

    add ability to print out stats for one or all songs

--- housekeeper/fantasydjhousekeeper/__main__.py
+++ housekeeper/fantasydjhousekeeper/__main__.py
@@ -1,7 +1,22 @@
 from . import spotify
 import fantasydj
+import argparse
 
-sp = spotify.Spotify()
-print(sp.spotify.me())
+parser = argparse.ArgumentParser(description='FantasyDJ nightly housekeeping process')
+parser.add_argument('-t', '--track', help="the spotify track id of a song for which you wish to get stats")
+parser.add_argument('-a', '--alltracks', action="store_true", help="get all stats, grouped by song")
+args = parser.parse_args()
 
-fantasydj.update_song_stats(sp)
+if args.alltracks:
+    fantasydj.print_song_stats()
+
+elif args.track is not None:
+    song = fantasydj.get_song_by_spotify_id(args.track)
+    if song is not None:
+        fantasydj.print_song_stats(song.id)
+    else:
+        print("No song returned for %s" % args.track)
+
+else:
+    sp = spotify.Spotify()
+    fantasydj.update_song_stats(sp)
--- housekeeper/fantasydjhousekeeper/fantasydj.py
+++ housekeeper/fantasydjhousekeeper/fantasydj.py
@@ -33,8 +33,7 @@ def get_active_leagues():
     ]
 
 
-def get_song(song_id):
-    fbsong = db.child('Songs/%s' % song_id).get()
+def __song_from_result(fbsong):
     val = fbsong.val()
 
     leagues = None
@@ -53,6 +52,21 @@ def get_song(song_id):
     return song
 
 
+def get_song(song_id):
+    return __song_from_result(db.child('Songs/%s' % song_id).get())
+
+
+def get_song_by_spotify_id(track_id):
+    res = db.child('Songs')\
+          .order_by_child('spotifyId')\
+          .start_at(track_id)\
+          .end_at(track_id)\
+          .get()
+    for fbsong in res.each():
+        return __song_from_result(fbsong)
+    return None
+
+
 def get_playlist(league_id, user_id):
     fbplaylist = db.child(
         'Leagues/%s/users/%s' % (league_id, user_id)
@@ -65,21 +79,55 @@ def get_playlist(league_id, user_id):
     return [get_song(song_id) for song_id in val.keys()]
 
 
-def get_song_stat(song_id, date_str):
+def __query_song_stats(song_id):
     stats = db.child('SongStats')\
               .order_by_child('songId')\
-              .start_at(song_id)\
-              .end_at(song_id)\
+              .start_at(str(song_id))\
+              .end_at(str(song_id))\
               .get()
-    for stat in stats.each():
-        val = stat.val()
-        if val['date'] == date_str:
-            return SongStat(
-                stat.key(),
-                val['songId'],
-                val['date'],
-                val['popularity']
-            )
+    return stats
+
+
+def get_all_stats_grouped_by_song_id(song_id=None):
+    stats_by_song = {}
+
+    query = db.child('SongStats').order_by_child('songId')
+
+    if song_id:
+        query.start_at(str(song_id)).end_at(str(song_id))
+
+    stats = query.get()
+    for fbstat in stats.each():
+        stat = __stat_from_result(fbstat)
+        if not stats_by_song.has_key(stat.songId):
+            stats_by_song[stat.songId] = []
+        stats_by_song[stat.songId].append(stat)
+
+    for key in stats_by_song.keys():
+        stats_by_song[key].sort(key=lambda stat: stat.date)
+
+    return stats_by_song
+
+
+def __stat_from_result(stat):
+    val = stat.val()
+    return SongStat(
+        stat.key(),
+        val['songId'],
+        get_date(val, 'date'),
+        val['popularity']
+    )
+
+
+def get_song_stats(song_id):
+    res = __query_song_stats(song_id)
+    return [__stat_from_result(stat) for stat in res.each()]
+
+
+def get_song_stat(song_id, date_str):
+    for stat in __query_song_stats(song_id).each():
+        if stat.val()['date'] == date_str:
+            return __stat_from_result(stat)
     return None
 
 
@@ -93,6 +141,18 @@ def add_song_stat(song_id, popularity):
 
     if not get_song_stat(song_id, date_str):
         db.child('SongStats').push(data)
+
+
+def print_song_stats(provided_song_id=None):
+    for song_id, stats in get_all_stats_grouped_by_song_id(provided_song_id).iteritems():
+        try:
+            song = get_song(song_id)
+            print('%s by %s (%s):' % (song.name, song.artist, song.spotifyId))
+        except TypeError:
+            print("song %s may not exist" % (song_id))
+
+        for stat in stats:
+            print('  %s: %s' % (stat.date, stat.popularity))
 
 
 def update_song_stats(spotify):
@@ -112,3 +172,4 @@ def update_song_stats(spotify):
                             popularity
                         ))
                         add_song_stat(song.id, popularity)
+

5bb284e6411803b128d616684bc3f7ba3576d6f2
Philip Oliver-Paull <oliverpa@mailbox.sc.edu>
2017-04-05 17:58:24 -0400

    search preview: pause stream on ionViewDidLeave, stops audio from playing when back button hit

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -57,6 +57,10 @@ export class SearchPage {
     console.log('Landed on Search Page');
   }
 
+  ionViewDidLeave() {
+    this.stream.pause();
+  }
+
 
   search() {
     if (this.query.length > 0) {
@@ -76,7 +80,7 @@ export class SearchPage {
   songPick(track) {
     this.currentTrack = track.id
     console.log("current track: ",this.currentTrack);
-    this.song = this.spotify.loadTrack(this.currentTrack).then(song => {
+    this.spotify.loadTrack(this.currentTrack).then(song => {
       console.log('song ' + song.preview_url + ' songpick success');
       this.url = song.preview_url;
       this.stream = new Audio(this.url);
